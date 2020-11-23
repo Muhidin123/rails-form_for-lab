7bdd4561415ca5465fa758bb2d4e0ce5376d503c
Muhidin <muhidinhukic1@gmail.com>
2020-11-22 18:52:26 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+for i in (1..5) do
+    SchoolClass.create(title: "Title #{i}", room_number: "#{i}")
+end
+
+
+for i in (1..5) do
+    Student.create(first_name: "First name #{i}", last_name: "Last name #{i}")
+end
+
